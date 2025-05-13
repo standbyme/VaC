b587f000de9de95b322cac1211798c52120e8481
Hongyu Cai <h.tsai@hotmail.com>
2025-05-13 17:00:21 -0400

    BeginningComponent

--- src/BeginningComponent/index.tsx
+++ src/BeginningComponent/index.tsx
@@ -1,45 +1,32 @@
-import { spring } from "remotion";
 import {
   AbsoluteFill,
   interpolate,
-  Sequence,
   useCurrentFrame,
   useVideoConfig,
 } from "remotion";
 import { z } from "zod";
 import { zColor } from "@remotion/zod-types";
 
-export const myCompSchema = z.object({
-  titleText: z.string(),
-  titleColor: zColor(),
-  logoColor1: zColor(),
-  logoColor2: zColor(),
+export const BeginningComponentSchema = z.object({
+  text: z.string(),
+  color: zColor(),
 });
 
-export const HelloWorld: React.FC<z.infer<typeof myCompSchema>> = ({
-  titleText: propOne,
-  titleColor: propTwo,
-  logoColor1,
-  logoColor2,
+export const BeginningComponent: React.FC<z.infer<typeof BeginningComponentSchema>> = ({
+  text,
+  color,
 }) => {
   const frame = useCurrentFrame();
-  const { durationInFrames, fps } = useVideoConfig();
+  const { durationInFrames } = useVideoConfig();
 
   // Animate from 0 to 1 after 25 frames
-  const logoTranslationProgress = spring({
-    frame: frame - 25,
-    fps,
-    config: {
-      damping: 100,
-    },
-  });
-
-  // Move the logo up by 150 pixels once the transition starts
-  const logoTranslation = interpolate(
-    logoTranslationProgress,
-    [0, 1],
-    [0, -150],
-  );
+  // const logoTranslationProgress = spring({
+  //   frame: frame - 25,
+  //   fps,
+  //   config: {
+  //     damping: 100,
+  //   },
+  // });
 
   // Fade out the animation at the end
   const opacity = interpolate(
@@ -56,18 +43,11 @@ export const HelloWorld: React.FC<z.infer<typeof myCompSchema>> = ({
   return (
     <AbsoluteFill style={{ backgroundColor: "white" }}>
       <AbsoluteFill style={{ opacity }}>
-        <AbsoluteFill style={{ transform: `translateY(${logoTranslation}px)` }}>
-          <Logo logoColor1={logoColor1} logoColor2={logoColor2} />
-        </AbsoluteFill>
-        {/* Sequences can shift the time for its children! */}
-        <Sequence from={35}>
-          <Title titleText={propOne} titleColor={propTwo} />
-        </Sequence>
-        {/* The subtitle will only enter on the 75th frame. */}
-        <Sequence from={75}>
-          <Subtitle />
-        </Sequence>
-      </AbsoluteFill>
+        <div style={{
+          fontSize: 100,
+          color: color,
+        }}>{text}</div>
+      </AbsoluteFill >
     </AbsoluteFill>
   );
 };
